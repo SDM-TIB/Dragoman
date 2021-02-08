426369f2d26be5c9739aa69fcc1dbc19cd258d5a
Sam J <samaneh.jozashoori@gmail.com>
2021-02-08 18:58:48 +0100

    updated

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 ## Running FunMap:
-python3 /Users/sam/Documents/GitHub/GenoDIS/FunMap/run_translator.py /Users/sam/Documents/GitHub/GenoDIS/FunMap/config-FunMap.ini
+python3 /mnt/e/GitHub/GenoKGC/FunMap/run_translator.py /mnt/e/GitHub/GenoKGC/FunMap/config-FunMap.ini
 ## Running SDM-RDFizer over the results:
-python3 /Users/sam/Desktop/TIB/SDM-RDFizer-master/rdfizer/run_rdfizer.py /Users/sam/Documents/GitHub/GenoDIS/config-SDM.ini
+python3 /mnt/e/TOOLS/SDM-RDFizer-master/rdfizer/run_rdfizer.py /mnt/e/GitHub/GenoKGC/config-SDM.ini
 
 
---
+++ runBoth.sh
@@ -0,0 +1,5 @@
+
+
+python3 /mnt/e/GitHub/GenoKGC/FunMap/run_translator.py /mnt/e/GitHub/GenoKGC/FunMap/config-FunMap.ini 
+
+python3 /mnt/e/TOOLS/SDM-RDFizer-master/rdfizer/run_rdfizer.py /mnt/e/GitHub/GenoKGC/config-SDM.ini
