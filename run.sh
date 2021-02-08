70571720da0e4b69d390110393e6fa1caa885731
samiscoding <samaneh.jozashoori@gmail.com>
2021-02-08 19:01:21 +0330

    updated!

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 ## Running FunMap:
 python3 /Users/sam/Documents/GitHub/GenoDIS/FunMap/run_translator.py /Users/sam/Documents/GitHub/GenoDIS/FunMap/config-FunMap.ini
 ## Running SDM-RDFizer over the results:
-python3 /Users/sam/Desktop/TIB/SDM-RDFizer-master/rdfizer/run_rdfizer.py /Users/sam/Desktop/TIB/SDM-RDFizer-master/rdfizer/configfile.ini
+python3 /Users/sam/Desktop/TIB/SDM-RDFizer-master/rdfizer/run_rdfizer.py /Users/sam/Documents/GitHub/GenoDIS/config-SDM.ini
 
 
